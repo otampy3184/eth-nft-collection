bccebfec7f4f070a3a1674d4a5fed3b1af25c38d
django_freeman <o.tampodo.t.18@gmail.com>
2022-10-15 21:09:07 +0900

    create contract

---
+++  collection-contract/contracts/NFTCollection.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extension/ERC721URIStorage.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/hardhat/console.sol";
+
+contract NFTCollection is ERC721URIStorage {
+    
+}
