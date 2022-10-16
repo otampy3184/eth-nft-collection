d3c87fc274f75de6c94f1484eddfe00b70bb5c00
django_freeman <o.tampodo.t.18@gmail.com>
2022-10-16 16:33:24 +0900

    deploy nft with svg metadata image

--- collection-contract/contracts/NFTCollection.sol
+++ collection-contract/contracts/NFTCollection.sol
@@ -10,17 +10,18 @@ contract NFTCollection is ERC721URIStorage {
 
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("NCollection", "CLLCT") {
-        console.log("construct success");
+    constructor() ERC721("SquareNFT", "SQUARE") {
+        console.log("This is my NFT contract.");
     }
 
     function makeNFT() public {
         uint256 newTokenId = _tokenIds.current();
 
         _safeMint(msg.sender, newTokenId);
-
-        _setTokenURI(newTokenId, "sample uri");
-
+        _setTokenURI(
+            newTokenId,
+            "data:application/json;base64,ewogICJuYW1lIjogIkVwaWNOZnRDcmVhdG9yIiwKICAiZGVzY3JpcHRpb24iOiAiVGhlIGhpZ2hseSBhY2NsYWltZWQgc3F1YXJlIGNvbGxlY3Rpb24iLAogICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0ZGF0YTppbWFnZS9zdmcreG1sO2Jhc2U2NCxQSE4yWnlCNGJXeHVjejBpYUhSMGNEb3ZMM2QzZHk1M015NXZjbWN2TWpBd01DOXpkbWNpSUhCeVpYTmxjblpsUVhOd1pXTjBVbUYwYVc4OUluaE5hVzVaVFdsdUlHMWxaWFFpSUhacFpYZENiM2c5SWpBZ01DQXpOVEFnTXpVd0lqNEtJQ0FnSUR4emRIbHNaVDR1WW1GelpTQjdJR1pwYkd3NklIZG9hWFJsT3lCbWIyNTBMV1poYldsc2VUb2djMlZ5YVdZN0lHWnZiblF0YzJsNlpUb2dNVFJ3ZURzZ2ZUd3ZjM1I1YkdVK0NpQWdJQ0E4Y21WamRDQjNhV1IwYUQwaU1UQXdKU0lnYUdWcFoyaDBQU0l4TURBbElpQm1hV3hzUFNKaWJHRmpheUlnTHo0S0lDQWdJRHgwWlhoMElIZzlJalV3SlNJZ2VUMGlOVEFsSWlCamJHRnpjejBpWW1GelpTSWdaRzl0YVc1aGJuUXRZbUZ6Wld4cGJtVTlJbTFwWkdSc1pTSWdkR1Y0ZEMxaGJtTm9iM0k5SW0xcFpHUnNaU0krUlhCcFkwNW1kRU55WldGMGIzSThMM1JsZUhRK0Nqd3ZjM1puUGc9PSIKfQ=="
+        );
         console.log("NFT w/ %s has been minted to %s", newTokenId, msg.sender);
 
         _tokenIds.increment();
