aed48809d7ffaf8ddbd14bc388d6053f8cb7e209
django_freeman <o.tampodo.t.18@gmail.com>
2022-10-17 22:56:39 +0900

    walletの接続を確認

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import React, {useEffect} from "react";
 
 // Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
 const TWITTER_HANDLE = 'あなたのTwitterのハンドルネームを貼り付けてください';
@@ -9,12 +9,28 @@ const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  // ブラウザにウォレットがつながっているか確認
+  const checkIfWalletIsConnected = () => {
+    const { ethereum } = window;
+    if (!ethereum){
+      console.log("please connect Metamask");
+      return;
+    }else{
+      console.log("found ethereum object", ethereum);
+    }
+  };
+
   // renderNotConnectedContainer メソッドを定義します。
   const renderNotConnectedContainer = () => (
     <button className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
+
+  // 画面更新時にwalletの接続状況を確認する
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="App">
